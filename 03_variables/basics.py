b6d41d7714fd6d6f1e5c7fc5d0406cc25aa5059e
Hit-Damani <damanihit@gmail.com>
2026-03-02 05:18:07 +0530

    understanding variables

---
+++ 03_variables/basics.py
@@ -0,0 +1,17 @@
+age = 12
+name = "Shyam"
+cash = 12.25
+
+#variables name should not start with a number
+#variables name should not contain any special characters except underscore(_)
+#variables name should not contain any spaces
+#variables name should not be a keyword
+
+#variables name should be descriptive
+#variables name should be in lowercase
+#variables names are case sensitive
+
+# a$$b = 12 --> this is not valid
+
+__ab = 12  # this is valid
+print(__ab)
