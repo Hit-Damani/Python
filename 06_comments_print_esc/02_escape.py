33777f08300422adf3af3212ba04009b15cd5cf8
Hit-Damani <damanihit@gmail.com>
2026-03-02 20:34:39 +0530

    escape

---
+++ 06_comments_print_esc/02_escape.py
@@ -0,0 +1,10 @@
+# \n # print a new line
+# \t # print a tab
+# \\ # print a backslash
+# \' # print a single quote
+# \" # print a double quote
+
+print("Hello World\nThis is a new line\\new line")
+print("Hello \" Shyam")
+print("Hello \' Shyam")
+print("Hello \t Shyam")
