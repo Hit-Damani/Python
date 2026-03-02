96ef0ea93f924769b2fa7f3fc2d20ceeec4ea613
Hit-Damani <damanihit@gmail.com>
2026-03-02 19:46:26 +0530

    data_types

---
+++ 03_variables/data_types.py
@@ -0,0 +1,23 @@
+age = 7
+print(type(age))
+
+name = "Shyam"
+print(type(name))
+
+cash = 12.25
+print(type(cash))
+
+print(type(True))
+print(type(False))
+
+# integers (int) are whole numbers (positive and negative)                   ex: 1, 2, 3
+# floats (float) are numbers with decimal points (positive and negative)     ex: 1.0, 2.0, 3.0
+# strings (str) are sequences of characters                                  ex: "Shyam" 
+# booleans (bool) are either True or False values                            ex: True, False
+# Lists (list) are ordered sequences of elements,mutable                     ex: [1, 2, 3]
+# Tuples (tuple) are ordered, immutable sequences of elements                ex: (1, 2, 3)
+# Sets (set) are unordered collections of unique elements                    ex: {1, 2, 3}
+# Dictionaries (dict) are unordered collections of key-value pairs           ex: {"name": "Shyam", "age": 7}
+
+# mutable means you can change the value of the variable after it has been created
+# immutable means you cannot change the value of the variable after it has been created
