a4c9de81dca52b4838ecd9b9bb545aa082f05110
Hit-Damani <damanihit@gmail.com>
2026-03-02 20:20:12 +0530

    examples of input function

---
+++ 05_user_input/02_example.py
@@ -0,0 +1,10 @@
+a = input("Enter number: ")
+# if we add directly without typecasting then it will throw error bcoz input function takes input as string
+# so we need to typecast it to int
+a = int(a)  
+print(a + 1) 
+
+
+b = int(input("Enter Second number: "))
+c = int(input("Enter Second number: "))
+print(b + c)
