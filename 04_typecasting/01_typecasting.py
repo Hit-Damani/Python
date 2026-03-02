b7daf9532b6c8cd99ff14b05dbc9743ef6031bc0
Hit-Damani <damanihit@gmail.com>
2026-03-02 20:04:19 +0530

    typecasting

---
+++ 04_typecasting/01_typecasting.py
@@ -0,0 +1,34 @@
+# typecasting is the process of converting one data type to another
+
+a = 12
+b = "12"
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+# convert b to an integer 
+c = int(b)
+print(c)
+print(type(c))
+
+d = 12.25
+
+# convert d to a string
+e = str(d)
+print(e)
+print(type(e))
+
+
+num_str = "12"
+num_int = int(num_str)
+print(type(num_int))
+print(type(num_str))
+
+
+pi = 3.14
+pi_int = int(pi)
+print(type(pi_int)) 
+print(pi_int)
