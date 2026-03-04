825cb7c259b06e7155992f64393526558b682e3b
Hit-Damani <damanihit@gmail.com>
2026-03-04 17:07:36 +0530

    logical operators

---
+++ 07_operators/03_Logical_operator.py
@@ -0,0 +1,22 @@
+a = end="\n"
+print("AND Operator")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False, a)
+
+print("OR Operator")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False, a)
+
+print("NOT Operator")
+print(not True)
+print(not False, a)
+
+print("XOR Operator")
+print(True ^ True)
+print(True ^ False)
+print(False ^ True)
+print(False ^ False)
