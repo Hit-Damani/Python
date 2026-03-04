1278c1de012c5c8c93872e4b59d5e3161828c515
Hit-Damani <damanihit@gmail.com>
2026-03-04 18:33:44 +0530

    pass understanding

---
+++ 09_Loops/06_pass.py
@@ -0,0 +1,6 @@
+a = 3
+
+for i in range(1, 21):
+    if i == 3:
+        pass # do nothing like a placeholder bcoz sometime peace of code is required but at that time we don't want to execute any code
+    print(i)
