ec7634ecb0cd53ecced6e732e6d0dbcceab8f0f9
Hit-Damani <damanihit@gmail.com>
2026-03-04 17:06:55 +0530

    arithmetic operators

---
+++ 07_operators/01_Arithmetic_operators.py
@@ -0,0 +1,13 @@
+a = 34
+b = 2
+
+# Arithmetic Operators
+print(" a + b = ", a + b)
+print(" a - b = ", a - b)
+print(" a * b = ", a * b)
+print(" a / b = ", a / b)
+print(" a % b = ", a % b)
+print(" a // b = ", a // b)  # floor division operator means integer division for example 34//2 = 17
+print(" a ** b = ", a ** b)  # a^b = a to the power of b
+
+
