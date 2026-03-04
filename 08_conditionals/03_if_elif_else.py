d227118d6d45436502ad795997a8a3f656de6679
Hit-Damani <damanihit@gmail.com>
2026-03-04 17:35:04 +0530

    if elif else statement

---
+++ 08_conditionals/03_if_elif_else.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your age: "))
+
+if(a > 18):
+    print("you can drive")
+    print("Thank you")
+elif(a == 18):
+    print("Lets schedule an interview")
+    print("Thank you")
+elif(a == 0):
+    print("Hey you are just born")    
+else:
+    print("you cannot drive")
+    print("sorry")    
