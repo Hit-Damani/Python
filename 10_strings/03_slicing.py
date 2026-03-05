6f1ef5dcd9dc1f1a09860df49d6244cf96e2690b
Hit-Damani <damanihit@gmail.com>
2026-03-05 20:41:27 +0530

    slicing of string

---
+++ 10_strings/03_slicing.py
@@ -0,0 +1,9 @@
+name = "Hit Damani"
+
+print(name[0:5])
+print(name[1:])
+print(name[0:-1])
+print(name[0:10:2]) #skip 1 character  (2-1) character 
+
+print(name[1:])  #starting from index 1 to end
+print(name[:5])  #starting from index 0 to 4
