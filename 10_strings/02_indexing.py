617160be0865315dad602ca6c937f81289e41ae6
Hit-Damani <damanihit@gmail.com>
2026-03-05 20:41:13 +0530

    indexing of string character

---
+++ 10_strings/02_indexing.py
@@ -0,0 +1,23 @@
+name = "Hit Damani"
+
+#                H   I  T     D  A  M  A  N  I
+#                0   1  2  3  4  5  6  7  8  9
+#               -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+     
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4], end="\n\n")
+
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+print(name[-6])
+
+# print(name[10]) This will throw an error
+
+
+
