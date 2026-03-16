33407fb4fc68232c5bb26e20570d511eb028daa4
Hit-Damani <damanihit@gmail.com>
2026-03-16 18:49:34 +0530

    basic set

---
+++ 14_sets/01_sets.py
@@ -0,0 +1,12 @@
+# sets are unordered and unique collections of elements, there is no duplicate elements
+
+a = {1, 2, 3, 4, 5}
+print(a, type(a))
+
+# Sets store elements using a hash table, not sequential positions like lists.
+# So elements are stored for fast lookup, not for ordering.
+
+l = list(a)
+print(l[0])
+
+
