d1b5ed21301e1abde2c28624c4da52c82658c6ee
Hit-Damani <damanihit@gmail.com>
2026-03-16 18:04:24 +0530

    List Comprehensions

---
+++ 12_lists/03_list_comprehension.py
@@ -0,0 +1,14 @@
+# creating a list containing the table of 5
+
+# table = []
+
+# for i in range(1, 11):
+#     table.append(5 * i)
+
+# print(table)
+
+table = [5 * i for i in range(1, 11)]
+print(table)
+
+squares = [i ** 2 for i in range(1, 11)]
+print(squares)
