3a3df20dfdcc81cdb8c8290445d456ae817c10ec
Hit-Damani <damanihit@gmail.com>
2026-03-16 19:10:05 +0530

    simple dictionary

---
+++ 15_dictionary/01_dictionary.py
@@ -0,0 +1,6 @@
+name_and_favourite_number = {"Shyam" : 7, "Diu": 12, "Mumbai": 25}
+print(name_and_favourite_number, type(name_and_favourite_number))
+
+print(name_and_favourite_number["Shyam"])
+name_and_favourite_number["Shyam"] = 1225
+print(name_and_favourite_number)
