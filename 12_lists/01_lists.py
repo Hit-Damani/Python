d98bf4dfdff92427c5bf7f43907af658ab716f10
Hit-Damani <damanihit@gmail.com>
2026-03-16 17:26:22 +0530

    simple list

---
+++ 12_lists/01_lists.py
@@ -0,0 +1,12 @@
+# lists are ordered, mutable (changeable) and can contain any type of data
+
+marks = [7, 12, 25, 1225, 2512]
+mixed = [7, "Shyam", 12, 25, 1225]
+
+print(marks)
+print(mixed)
+print(marks[2])
+print(mixed[1])
+print(marks[2 : 4])
+print(mixed[1 : 3])
+# print(marks[5])    # This will throw an error bcoz index out of bound
