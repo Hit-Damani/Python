b22e8b02be96d318ee7aace02928c17d0a00214a
Hit-Damani <damanihit@gmail.com>
2026-03-16 18:35:02 +0530

    method of tuple

---
+++ 13_tuples/03_tuple_methods.py
@@ -0,0 +1,9 @@
+counting = (1, 2, 1, 25, 12)
+print(counting.count(1))
+
+print(counting.index(1)) # returns the index of first occurence if there are more then one
+
+# why use tuples??
+# tuples are faster than lists
+# tuples are used as dictionary keys
+# safe from unintentional changes
